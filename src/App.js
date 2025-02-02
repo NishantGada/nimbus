e0ba6b145c385707d7c7e7e7a4d1830b4357798b
NishantGada <nishant.gada160@gmail.com>
2025-02-01 22:21:51 -0500

    refactor: included tasks in app routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css'
-import Sidebar from './components/Sidebar';
+import Sidebar from './components/Sidebar/Sidebar';
 import Home from './sections/Home/Home';
 import Profile from './sections/Profile/Profile';
 import Settings from './sections/Settings/Settings';
+import Tasks from "./sections/Tasks/Tasks";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/tasks" element={<Tasks />} />
           </Routes>
         </div>
       </div>
