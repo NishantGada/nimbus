12ae23a384b0b9cbedc4b012f86c5d31c9dc0529
NishantGada <nishant.gada160@gmail.com>
2025-02-03 23:53:52 -0500

    refactor: notes grid edit & delete functionality + UI

--- src/sections/Notes/NotesGrid.js
+++ src/sections/Notes/NotesGrid.js
@@ -1,13 +1,27 @@
-import React from "react";
-import "./NotesGrid.css"; // External CSS file for styling
+import React, { useState } from "react";
+import EditNoteModal from "./EditNoteModal";
+import "./NotesGrid.css";
+import dustbinIcon from "./images/dustbin.png";
+import editIcon from "./images/edit.png";
 
-const NotesGrid = ({ notes }) => {
+const NotesGrid = ({ notes, deleteNoteAPI }) => {
+    const [selectedNote, setSelectedNote] = useState(null);
+
     return (
         <div className="notes-container">
             {notes.length > 0 ? (
                 notes.map((note) => (
-                    <div className="note-card" key={note.id}>
-                        <h3 className="note-title">{note.title}</h3>
+                    <div
+                        className="note-card"
+                        key={note.id}
+                    >
+                        <div className="note-card-header">
+                            <h3 className="note-title">{note.title}</h3>
+                            <div>
+                                <img src={editIcon} onClick={() => setSelectedNote(note)} />
+                                <img src={dustbinIcon} onClick={() => deleteNoteAPI(note.note_id)} />
+                            </div>
+                        </div>
                         <p className="note-description">{note.description}</p>
                         <div className="note-footer">
                             <span>{note.category}</span>
@@ -17,6 +31,13 @@ const NotesGrid = ({ notes }) => {
                 ))
             ) : (
                 <p className="empty-text">No notes available</p>
+            )}
+
+            {selectedNote && (
+                <EditNoteModal
+                    note={selectedNote}
+                    onClose={() => setSelectedNote(null)}
+                />
             )}
         </div>
     );
